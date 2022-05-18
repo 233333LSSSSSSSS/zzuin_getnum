7b45badb8c601bde679c7c4c5162824b0fff004a
23333333LSSSSSS <70122866+233333LSSSSSSSS@users.noreply.github.com>
2022-05-18 15:42:59 +0800

    Update getnum.py

--- getnum.py
+++ getnum.py
@@ -24,7 +24,7 @@ header2 = {"Referer": "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login",
            "Host": "jksb.v.zzu.edu.cn"
            }
 
-post_data = {"uid": "", "upw": ".", "smbtn": "领取郑州大学通行码", "hh28": "664", "fun2": "s",
+post_data = {"uid": "20177710735", "upw": "11042513", "smbtn": "领取郑州大学通行码", "hh28": "664", "fun2": "s",
              "door": "63215i"}
 
 # verify_path = "/etc/ssl/certs"
@@ -68,53 +68,4 @@ def find_num(html):
     return num
 
 
-# post at the first time
-def post_url():
-    session = requests.Session()
-    html = session.post('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login', data=post_data, headers=header,
-                        verify=verify_path)
-    # html.encoding = 'utf-8'
-    # print(html.text)
-    return html
-
-
-# get at the first time
-def get_url(url):
-    session = requests.Session()
-    html = session.get(url, headers=header2, verify=verify_path)
-    return html
-
-
-def send_to_wecom(text, wecom_cid, wecom_aid, wecom_secret, wecom_touid):
-    get_token_url = f"https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={wecom_cid}&corpsecret={wecom_secret}"
-    response = requests.get(get_token_url).content
-    access_token = json.loads(response).get('access_token')
-    if access_token and len(access_token) > 0:
-        send_msg_url = f'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={access_token}'
-        data = {
-            "touser": wecom_touid,
-            "agentid": wecom_aid,
-            "msgtype": "text",
-            "text": {
-                "content": text
-            },
-            "duplicate_check_interval": 600
-        }
-        response = requests.post(send_msg_url, data=json.dumps(data)).content
-        return response
-    else:
-        return False
-
-
-if __name__ == '__main__':
-    post_re = post_url()
-    window_url = url_window(post_re)
-    get_re = get_url(window_url[0])
-    code = find_num(get_re)
-    if code is not None:
-        print("获取成功")
-        send_to_wecom("")
-    else:
-        print("失败")
-else:
-    print("失败")
+    
